67493d7df4b128fb02cddd0f2aface2275ea15bb
David Amunga <david.onyimbo@gmail.com>
2019-08-07 17:38:05 +0300

    Added Docker Compose File

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Binomial Nomenclature
         </a>
       </header>
     </div>
